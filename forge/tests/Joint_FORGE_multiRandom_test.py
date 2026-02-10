d66093e2159ea895c97dd6e1eb428efd5d51c97f
Sree Ramchandra Murthy Peela <59723039+sreerampeela@users.noreply.github.com>
2026-02-10 20:57:02 +0530

    updated imports

--- forge/tests/Joint_FORGE_multiRandom_test.py
+++ forge/tests/Joint_FORGE_multiRandom_test.py
@@ -3,8 +3,7 @@ import os
 import pandas as pd
 import numpy as np
 from scipy.stats import pearsonr, spearmanr, rankdata
-from forge_class_instance3 import *
-# from forge_ind_model_combined_optuna import *
+from JointFORGE import *
 from numthreads import set_num_threads
 from pyinstrument import Profiler
 from memory_profiler import memory_usage
