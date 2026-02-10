2f34e2578bc1fd8109d4a1a39622b75e4ebf89e2
Sree Ramchandra Murthy Peela <59723039+sreerampeela@users.noreply.github.com>
2026-02-10 21:05:19 +0530

    update file paths for inputs and outputs

--- forge/tests/Joint_FORGE_multiRandom_test.py
+++ forge/tests/Joint_FORGE_multiRandom_test.py
@@ -13,24 +13,15 @@ import gc
 # set max number of threads to 4 for the code here
 set_num_threads(4)
 
-exp_path = "/home/nilabjab/cancer_dependency_project_nilabja/cancer_dependency_project/Approach3_Latent_factor/Fresh_FORGE/Data/Exp.csv"
-dep_path = "/home/nilabjab/cancer_dependency_project_nilabja/cancer_dependency_project/Approach3_Latent_factor/Fresh_FORGE/Data/Dep.csv"
-ic50_path = "/home/nilabjab/cancer_dependency_project_nilabja/cancer_dependency_project/Approach3_Latent_factor/Fresh_FORGE/Data/Creammist_common_ic50.csv"
+exp_path = "./Data/Exp.csv"
+dep_path = "./Data/Dep.csv"
+ic50_path = "./Data/Creammist_common_ic50.csv"
 
-drug_target_data = pd.read_csv('/home/sreeramp/cancer_dependency_project/nilabja/Approach3_Latent_factor/Fresh_FORGE/Data/Drug_target_data.csv', 
+drug_target_data = pd.read_csv('./Data/Drug_target_data.csv', 
                                header=0, index_col = 0)
 # drug_target_data.head()
 key_drugs = ['AZD8931']
 seed_instances = [42, 586231, 321456, 227745, 228796, 12587, 23698, 11111222]
-# with open('low_sampleSize_drugs.txt', 'r') as f:
-#   ss_low = [i.rstrip().strip() for i in f.readlines()]
-
-# with open('high_sampleSize_drugs.txt', 'r') as f:
-#   ss_high = [i.rstrip().strip() for i in f.readlines()]
-# key_drugs = ['ERLOTINIB', 'IMATINIB', 'MK-2206', 'TIVANTINIB', 'ULIXERTINIB',
-#                  'UPROSERTIB', 'BMS-754807', 'DABRAFENIB', 'DAPORINAD']
-# key_drugs = ss_low[:]
-# key_drugs.extend(ss_high)
 dep_data_full = pd.read_csv(dep_path, header = 0, index_col = 0)
 dep_data_genes = dep_data_full.columns.tolist()
 
@@ -41,7 +32,7 @@ exp_data_genes = exp_data.columns.tolist()
 
 #### Run the full pipeline for all drug-target pairs
 # with logging file output
-model_out_dir = '/home/sreeramp/cancer_dependency_project/nilabja/Approach3_Latent_factor/git_repo/Models/multiSeed_optuna_indModels'
+model_out_dir = './Models/multiSeed_optuna_indModels'
 for rand_seed in seed_instances:
   for drug in key_drugs:
     target_genes = drug_target_data.loc[drug, 'Target'].split(',')
@@ -51,9 +42,7 @@ for rand_seed in seed_instances:
       if os.path.exists(model_path):
         print(f'Model for {drug}-{target} pair already exists. Skipping it..')
         continue
-    # elif os.path.exists(f'{drug}_{target}_forgeModel_rankedHP_17122025_voting.pkl'):
-    #   print('Hyperparameter tuning performed..retraining based on best model')
-      
+       
       elif (target in dep_data_genes) & (target in exp_data_genes) :
         print(f'Building FORGE for {drug}-{target} pair..')
         log_file = os.path.join(model_out_dir, f'{drug}_{target}_logfile_{rand_seed}.txt')
@@ -62,7 +51,7 @@ for rand_seed in seed_instances:
         t1 = FORGE(exp_path=exp_path, dependency_path=dep_path, ic50_path=ic50_path,
                drug_name=drug, target_name=target, log_file=log_file, overwrite=True)
         print(f'Running FORGE for {drug}-{target} pair (Optuna tuning included) using seed: {rand_seed}..')
-              # --- Track memory usage during run_Pipeline ---
+             
         t1.run_Pipeline(n_splits=5, seed_val=rand_seed, tuning_epochs=500,
                                              training_epochs=1000, 
                                              model_path=model_path,
